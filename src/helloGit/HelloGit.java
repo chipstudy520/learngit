b2c3c8611748a033fc9cda6d0f88b78b96819a5a
chipstudy <likai@3gosc.com>
2017-02-16 16:44:18 +0800

    and spring application

--- src/helloGit/HelloGit.java
+++ src/helloGit/HelloGit.java
@@ -1,11 +1,36 @@
 package helloGit;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello git ");
-		System.out.println("hello print");
+		ApplicationContext context = new ClassPathXmlApplicationContext("HelloXml.xml");
+		HelloSpring helloSpring = (HelloSpring) context.getBean("hellospring");
+		helloSpring.print();
 	}
 
 }
+
+/*class HelloSpring{
+	
+	int x= 0;
+	
+	
+	public int getX() {
+		return x;
+	}
+
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+
+	public void print(){
+		System.out.println(" Hello Spring x = " +x);
+	}
+	
+}*/
---
+++ src/helloGit/HelloSpring.java
@@ -0,0 +1,19 @@
+package helloGit;
+
+public class HelloSpring {
+
+	int x = 0;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void print() {
+		System.out.println(" Hello Spring x = " + x);
+	}
+
+}
